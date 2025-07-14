61fe12f2672d8fb4b948774b0fd0ad5b21f903b8
techenthusiast321 <12212140@nitkkr.ac.in>
2025-07-14 16:34:21 +0530

    SignUp Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import {Routes} from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import SignUp from './pages/SignUp.jsx'
 import SignIn from './pages/SignIn.jsx'
+
+export const serverUrl="http://localhost:8000"
 const App = () => {
   return (
     <Routes>
